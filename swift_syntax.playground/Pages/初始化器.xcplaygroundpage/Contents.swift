43fd95bd559a8bfe5f0afdf54389d7aae4b5c3ec
银_色_黎_明 <25856163434@qq.com>
2021-08-27 10:16:36 +0800

    add and modify file

---
+++ swift_syntax.playground/Pages/初始化器.xcplaygroundpage/Contents.swift
@@ -0,0 +1,24 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var greeting = "Hello, playground"
+
+// 类有2种初始化器
+// 1. 指定初始化器
+// 2. 便捷初始化器
+class Size {
+    var age: Int?
+    
+    init(age: Int) {
+        
+    }
+    
+    
+}
+
+
+
+
+
+//: [Next](@next)
